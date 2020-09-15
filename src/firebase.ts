c04272baa414b370f1be034bc41f348de44ca75a
thepedroferrari <pedro@pedroferrari.com>
2020-09-15 21:19:03 +0200

    Install Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const config = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DBURL,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_MSID,
+  appId: process.env.REACT_APP_APPID
+};
+
+firebase.initializeApp(config);
+
+const databaseRef = firebase.database().ref();
+export const todosRef = databaseRef.child('leaderboard')
+export default firebase;
+
