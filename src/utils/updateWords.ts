84e4a9019ad23fb4f3f09ae110109ae07e09895d
thepedroferrari <pedro@pedroferrari.com>
2020-09-17 02:19:34 +0200

    Modify function so possible for multiple languages

--- src/utils/updateWords.ts
+++ src/utils/updateWords.ts
@@ -1,22 +1,27 @@
 import { textBreaker, getRandomInt } from './utils';
 import english from '../languages/english';
+import swedish from '../languages/swedish';
 
 export const NUMBER_OF_WORDS = 5;
+type language = 'en' | 'sv' | 'pt';
 
-const words = textBreaker(english);
-const length = words.length;
-const getWord = () => words[getRandomInt(length)];
+const words = (lang: language) => {
+  if (lang === 'sv') return textBreaker(swedish)
+  return textBreaker(english)
+};
+
+const getWord = (lang: language) => words(lang)[getRandomInt(words(lang).length)];
 
 type INextWords = string[];
 
-const updateWords = (prevWords: INextWords = []): INextWords => {
+const updateWords = (prevWords: INextWords = [], lang: language = 'en'): INextWords => {
   if (prevWords.length) {
     const nextWords = prevWords.slice(1, NUMBER_OF_WORDS);
-    const newWord = getWord();
+    const newWord = getWord(lang);
     nextWords.push(newWord);
     return nextWords;
   }
-  return [...Array(NUMBER_OF_WORDS)].map(() => getWord());
+  return [...Array(NUMBER_OF_WORDS)].map(() => getWord(lang));
 }
 
 export default updateWords;
